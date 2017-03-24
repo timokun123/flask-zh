fa433ede11f3cfddb054987b5feae2da847d4f94
HAN <khaliltfan@163.com>
2017-03-24 10:40:19 +0800

    add virtualenv flask-zh 3b with flask-bootstrap

--- flask-zh/Code/hello.py
+++ flask-zh/Code/hello.py
@@ -1,11 +1,12 @@
 #coding = utf-8
 from flask import Flask,render_template
 from flask_script import Manager
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
 
 manager = Manager(app)
-
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
