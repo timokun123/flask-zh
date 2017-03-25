f76c5770cf1c693d7fc31989fd68d9fa3a469c5b
HAN <khaliltfan@163.com>
2017-03-25 11:45:58 +0800

    add virtualenv flask-zh 4c

--- flask-zh/Code/hello.py
+++ flask-zh/Code/hello.py
@@ -1,6 +1,6 @@
  #!/usr/bin/python
  #-*-coding:utf-8 -*-
-from flask import Flask,render_template
+from flask import Flask,render_template,session,redirect,url_for,flash
 from flask_script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
@@ -22,12 +22,14 @@ class NameForm(Form):
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html',form = form,name = name,current_time=datetime.utcnow())
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html',form = form,name = session.get('name'),current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
