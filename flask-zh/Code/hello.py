3271ee13dc4641ee80d557308f9460abeee66c8a
HAN <khaliltfan@163.com>
2017-03-28 11:19:42 +0800

    add virtualenv flask-zh

--- flask-zh/Code/hello.py
+++ flask-zh/Code/hello.py
@@ -62,7 +62,6 @@ def index():
         return redirect(url_for('index'))
     return render_template("index.html",form=form,name=session.get('name'),known=session.get('known',False),current_time=datetime.utcnow())
 
-
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html',name=name)
