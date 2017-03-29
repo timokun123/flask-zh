744dbb5415bbec5513e7653f1b6399fac30f7459
HAN <khaliltfan@163.com>
2017-03-29 13:38:43 +0800

    add virtualenv flask-zh 7b complete views.py

--- flask-zh/Code/app/main/views.py
+++ flask-zh/Code/app/main/views.py
@@ -13,7 +13,16 @@ from ..models import User
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        #....
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_email(app.config['FLASKY_ADMIN'],'New User','mail/new_user',user=user)
+        else:
+            session['known'] = True
+        session['name']=form.name.data
         return redirect(url_for('.index'))
     return render_template('index.html',form=form,name=session.get('name'),known=session.get('known',False),current_time=datetime.utcnow())
 
