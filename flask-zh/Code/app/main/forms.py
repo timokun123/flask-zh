d85f488f2ddfab2c9a5f558a8ef3bab3a9cd1b87
HAN <khaliltfan@163.com>
2017-03-31 10:46:47 +0800

    add virtualenv flask-zh 8c

--- flask-zh/Code/app/main/forms.py
+++ flask-zh/Code/app/main/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms.validators import DataRequired
 
 
 class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[Required()])
+    name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
